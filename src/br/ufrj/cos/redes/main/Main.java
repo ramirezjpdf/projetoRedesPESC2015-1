8c620b295e596758e366a3ea5d79253cae298a23
ramirezjpdf <ramirez.jpdf@ymail.com>
2015-06-20 02:03:13 -0300

    add Main.java

--- src/br/ufrj/cos/redes/main/Main.java
+++ src/br/ufrj/cos/redes/main/Main.java
@@ -2,9 +2,6 @@ package br.ufrj.cos.redes.main;
 
 public class Main {
 	public static void main(String[] args) {
-		long a = 100;
-		long b = 15;
 		
-		System.out.println("a / b = " + a / b);
 	}
 }
