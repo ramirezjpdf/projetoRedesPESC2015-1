c4eb4748cee6dfbf61d1fbaaefe354a663f8d75d
freitaspedro <pfreitas@id.uff.br>
2015-06-27 00:27:32 -0300

    example of Receiver for DelayLossSimulator

---
+++ src/br/ufrj/cos/redes/delayLossSimulator/ReceiverForDelayLossSimulatorExample.java
@@ -0,0 +1,35 @@
+package br.ufrj.cos.redes.delayLossSimulator;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
+import br.ufrj.cos.redes.packet.InitPacket;
+
+public class ReceiverForDelayLossSimulatorExample {
+
+	public static void main(String[] args) throws IOException, ClassNotFoundException {
+		String REQUESTED_FILE_NAME = "C:\\Users\\Pedro Freitas\\Desktop\\mac.pdf";
+		InetAddress SERVER_ADDRESS = InetAddress.getLoopbackAddress();
+		int SERVER_PORT = 29920;
+		
+		ByteArrayOutputStream byteOstream = new ByteArrayOutputStream();
+		ObjectOutputStream objOStream = new ObjectOutputStream(byteOstream);
+		
+		InitPacket initPkt = new InitPacket(InitPacket.INIT_MSG, REQUESTED_FILE_NAME);
+		objOStream.writeObject(initPkt);
+		byte[] sendBytes = byteOstream.toByteArray();
+		
+		DatagramPacket sendPkt = new DatagramPacket(sendBytes, sendBytes.length, SERVER_ADDRESS, SERVER_PORT);
+		DatagramSocket clientSocket = new DatagramSocket();
+				
+		System.out.println("Requesting file " + REQUESTED_FILE_NAME);
+		clientSocket.send(sendPkt);
+		
+		DelayLossSimulator.receive(clientSocket);
+		
+	}
+}
