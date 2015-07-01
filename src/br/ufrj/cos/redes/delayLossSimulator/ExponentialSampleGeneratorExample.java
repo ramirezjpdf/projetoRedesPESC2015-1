6292a210321925702621cb0b3da55b314ab66dfe
freitaspedro <pfreitas@id.uff.br>
2015-07-01 16:31:36 -0300

    defined lambda

--- src/br/ufrj/cos/redes/delayLossSimulator/ExponentialSampleGeneratorExample.java
+++ src/br/ufrj/cos/redes/delayLossSimulator/ExponentialSampleGeneratorExample.java
@@ -3,10 +3,10 @@ package br.ufrj.cos.redes.delayLossSimulator;
 public class ExponentialSampleGeneratorExample {
 	public static void main(String[] args) {
 		//E[X] = 1/lambda for exponential random variables
-		double LAMBDA = 50;
+		double LAMBDA = 5;
 		ExponentialSampleGenerator generator = new ExponentialSampleGenerator(LAMBDA);
 		for (int i = 0; i < 500; i++) {
-			System.out.println("Sample generated = " + generator.getSample());
+			System.out.println("Sample generated = " + (long) (generator.getSample()*1000));
 		}
 	}
 }
