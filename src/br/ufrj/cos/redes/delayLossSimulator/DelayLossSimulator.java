a4a6de20eb6dd44abbeb093ad947e0619bb4fb50
freitaspedro <pfreitas@id.uff.br>
2015-07-01 16:32:52 -0300

    DelayLossSimulator finished

--- src/br/ufrj/cos/redes/delayLossSimulator/DelayLossSimulator.java
+++ src/br/ufrj/cos/redes/delayLossSimulator/DelayLossSimulator.java
@@ -5,13 +5,9 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.Map;
-import java.util.Map.Entry;
+import java.util.Calendar;
 import java.util.Timer;
 import java.util.TimerTask;
-import java.util.TreeMap;
 
 import br.ufrj.cos.redes.fileAccess.Chunk;
 import br.ufrj.cos.redes.packet.EndPacket;
@@ -22,17 +18,19 @@ public class DelayLossSimulator {
 	
 	private double F;
 	private double LAMBDA;
-	private double RTT;
+	private long RTT;
+	private Buffer buffer;
+	private ChunkList chunkList;
 
-	public DelayLossSimulator(double F, double LAMBDA, double RTT) {
+	public DelayLossSimulator(double F, double LAMBDA, long RTT, Buffer buffer) {
 		this.F = F;
 		this.LAMBDA = LAMBDA;
 		this.RTT = RTT;
+		this.buffer = buffer;
+		this.chunkList = new ChunkList();
 	}
 
-	public void receive(DatagramSocket clientSocket, Buffer buffer) throws IOException, ClassNotFoundException {		
-		TreeMap<Double, Chunk> tnXChunk = new TreeMap<Double, Chunk>();
-		
+	public void receive(DatagramSocket clientSocket) throws IOException, ClassNotFoundException {			
 		boolean keep = true;
 		
 		try {
@@ -56,7 +54,10 @@ public class DelayLossSimulator {
 						System.out.println("Error in init object");
 					}					
 					keep = !endPkt.getSendedEndMsg().equals(EndPacket.END_MSG);	
-					buffer.alertEnd();
+
+					System.out.println("waiting for last chunk in simulator...");
+					do {} while(!chunkList.isOver());
+					buffer.alertEnd();				
 				}
 				
 				if(keep) {				
@@ -67,52 +68,35 @@ public class DelayLossSimulator {
 					
 					if(chunk.isAvailable()) {
 						ExponentialSampleGenerator generator = new ExponentialSampleGenerator(LAMBDA);
-						double x = generator.getSample();
+						long x = (long) (generator.getSample()*1000);
 						
-						double tn = pkg.getTimeStamp() + RTT/2 + x;		
-						pkg.setTimeStamp(tn);
+						long tn = chunk.getTransTimeStamp() + RTT/2 + x;	
+						chunk.setRecTimeStamp(tn);
 						
-						tnXChunk.put(pkg.getTimeStamp(), chunk);
-						
-						int firstTn = (int) Math.round(tnXChunk.firstKey());
-						Entry firstEntry = tnXChunk.firstEntry();
+						chunkList.add(tn, chunk);
 						
 						Timer timer = new Timer();
 						timer.schedule(new TimerTask() {
 							
 							@Override
 							public void run() {
-								buffer.add((Chunk) tnXChunk.firstEntry().getValue());								
-								tnXChunk.remove(firstEntry);
-								System.out.println("chunk number " + ((Chunk) firstEntry).getSeqNum() + " send to buffer at " + firstTn + "ms");
-								timer.cancel();
+								System.out.println("tn = " + tn);
+//								chunkList.print();
+								buffer.add((Chunk) chunkList.getChunk(tn));
 							}
-						}, firstTn, firstTn);
+						}, tn);
 						
 						
 					} else {
-						System.out.println("chunk number " + chunk.getSeqNum() + " was lost");
+						System.out.println("chunk with number " + chunk.getSeqNum() + " was lost in receive size simulator");
 					}					
 				}				
 			}
-		
-			printMap(tnXChunk);
-			System.out.println("number of chunks received: " + tnXChunk.size());
-			System.out.println("All chunks received");
 			
 		} finally {
 			clientSocket.close();
 		}
 		
 	}	
-	
-	public static void printMap(Map map) {
-		Iterator iterator = map.entrySet().iterator(); 
-		while(iterator.hasNext()) {
-			Map.Entry entry = (Map.Entry) iterator.next();
-			System.out.println("List of cunks received");
-			System.out.println("TimeStamp: " + entry.getKey() + " SeqNum: " + ((Chunk) entry.getValue()).getSeqNum());
-		}
-	}
 	
 }
