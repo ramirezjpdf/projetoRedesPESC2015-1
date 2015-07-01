137862f7387fbd18796d9f8f38922a38afe23034
freitaspedro <pfreitas@id.uff.br>
2015-07-01 16:34:00 -0300

    variables for timestamp

--- src/br/ufrj/cos/redes/fileAccess/Chunk.java
+++ src/br/ufrj/cos/redes/fileAccess/Chunk.java
@@ -11,7 +11,12 @@ public class Chunk implements Comparable<Chunk>, Serializable {
 	private byte[] bytes;
 	private int actualChunkLength;
 	private boolean available;
+	private long transTimeStamp;
+	private long recTimeStamp;
+	private long playedTimeStamp;
 	
+	
+
 	public Chunk(int chunkLength) {
 		bytes = new byte[chunkLength];
 	}
@@ -58,5 +63,28 @@ public class Chunk implements Comparable<Chunk>, Serializable {
 		this.available = available;
 	}
 	
+	public long getTransTimeStamp() {
+		return transTimeStamp;
+	}
+	
+	public void setTransTimeStamp(long transTimeStamp) {
+		this.transTimeStamp = transTimeStamp;
+	}
+	
+	public long getRecTimeStamp() {
+		return recTimeStamp;
+	}
+	
+	public void setRecTimeStamp(long recTimeStamp) {
+		this.recTimeStamp = recTimeStamp;
+	}
+	
+	public long getPlayedTimeStamp() {
+		return playedTimeStamp;
+	}
+	
+	public void setPlayedTimeStamp(long playedTimeStamp) {
+		this.playedTimeStamp = playedTimeStamp;
+	}
 }
 
