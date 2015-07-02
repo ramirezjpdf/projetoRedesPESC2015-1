33a833d6dc047545d17389e08099c0d756478bee
freitaspedro <pfreitas@id.uff.br>
2015-07-02 17:09:46 -0300

    HashMap replacing TreeMap

--- src/br/ufrj/cos/redes/delayLossSimulator/ChunkList.java
+++ src/br/ufrj/cos/redes/delayLossSimulator/ChunkList.java
@@ -1,19 +1,18 @@
 package br.ufrj.cos.redes.delayLossSimulator;
 
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
-import java.util.SortedMap;
-import java.util.TreeMap;
 
 import br.ufrj.cos.redes.fileAccess.Chunk;
 
 public class ChunkList {
 
-	private SortedMap<Long, Chunk> chunkList;
+	private Map<Long, Chunk> chunkList;
 	
 	public ChunkList() {
-		chunkList = Collections.synchronizedSortedMap(new TreeMap<Long, Chunk>());
+		chunkList = Collections.synchronizedMap(new HashMap<Long, Chunk>());
 	}
 	
 	public void add(long tn, Chunk chunk) {
@@ -26,27 +25,21 @@ public class ChunkList {
 		
 		return chunk;		
 	}
-	
-	public long getTime() {		
-		return chunkList.firstKey();		
-	}
-	
+		
 	public boolean isOver() {
 		return chunkList.isEmpty();
 	}	
 	
 	public void print() {
 		Iterator iterator = chunkList.entrySet().iterator(); 
-		System.out.println("List of cunks received:");
+		System.out.println("List of chunks passed in simulator:");
 		while(iterator.hasNext()) {
 			Map.Entry entry = (Map.Entry) iterator.next();
-			System.out.println("(timeStamp = " + entry.getKey() + ", seqNum = " + ((Chunk) entry.getValue()).getSeqNum() + ")");
+			System.out.println("(tn = " + entry.getKey() + ", seqNum = " + ((Chunk) entry.getValue()).getSeqNum() + ")");
 		}		
 	}
 	
-	public int size() {
-		return chunkList.size();
-	}
+	
 }
 
 
