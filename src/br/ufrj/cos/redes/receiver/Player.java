45ebf70c1ea463a9344103e46a217d3ed7619de3
ramirezjpdf <ramirez.jpdf@ymail.com>
2015-07-02 11:23:27 -0300

    implement log action

--- src/br/ufrj/cos/redes/receiver/Player.java
+++ src/br/ufrj/cos/redes/receiver/Player.java
@@ -2,21 +2,40 @@ package br.ufrj.cos.redes.receiver;
 
 import java.io.IOException;
 import java.io.OutputStream;
+import java.io.PrintWriter;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
 import br.ufrj.cos.redes.fileAccess.Chunk;
 
 public class Player {
-	private OutputStream ostream;
+	private OutputStream playerOStream;
+	private PrintWriter logWriter;
+	private SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSz");
 	
-	public Player(OutputStream ostream) {
-		this.ostream = ostream; 
+	public Player(OutputStream ostream, OutputStream logStream) {
+		this.playerOStream = ostream;
+		this.logWriter = new PrintWriter(logStream);
+		initLog();
 	}
 	
 	public void play(Chunk chunk) throws IOException {
-		SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSz");
-		System.out.println(formatter.format(Calendar.getInstance().getTime()) + ": Playing chunk with seqNum " + chunk.getSeqNum());
-		ostream.write(chunk.getBytes(), 0, chunk.getActualChunkLength());
+		Calendar calendar = Calendar.getInstance();
+		chunk.setPlayedTimeStamp(calendar.getTimeInMillis());
+		System.out.println(formatter.format(calendar.getTime()) + ": Playing chunk with seqNum " + chunk.getSeqNum());
+		playerOStream.write(chunk.getBytes(), 0, chunk.getActualChunkLength());
+		log(chunk);
+	}
+	
+	private void initLog() {
+		logWriter.println("seqNum,transTimestamp,recvTimestamp,playedTimestamp");
+	}
+	
+	private void log(Chunk chunk) {
+		logWriter.println(chunk.getSeqNum() + "," + 
+						  chunk.getTransTimeStamp() + "," +
+						  chunk.getRecTimeStamp() + "," + 
+						  chunk.getPlayedTimeStamp());
+		logWriter.flush();
 	}
 }
