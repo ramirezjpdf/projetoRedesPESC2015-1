d29c0cd51a49a8c637f095a10657644abbccff68
ramirezjpdf <ramirez.jpdf@ymail.com>
2015-07-04 01:18:03 -0300

    add attr for server addr and server socket port

--- src/br/ufrj/cos/redes/packet/EndPacket.java
+++ src/br/ufrj/cos/redes/packet/EndPacket.java
@@ -1,6 +1,7 @@
 package br.ufrj.cos.redes.packet;
 
 import java.io.Serializable;
+import java.net.InetAddress;
 
 public class EndPacket implements Serializable {
 	/**
@@ -11,6 +12,8 @@ public class EndPacket implements Serializable {
 	public static final String END_MSG = "FINISHED";
 	
 	private String sendedEndMsg;
+	private InetAddress serverAddress;
+	private int serverAckPort;
 
 	public EndPacket(String sendedEndMsg) {
 		this.sendedEndMsg = sendedEndMsg;
@@ -18,5 +21,21 @@ public class EndPacket implements Serializable {
 
 	public String getSendedEndMsg() {
 		return this.sendedEndMsg;
+	}
+
+	public InetAddress getServerAddress() {
+		return serverAddress;
+	}
+
+	public void setServerAddress(InetAddress serverAddress) {
+		this.serverAddress = serverAddress;
+	}
+
+	public int getServerAckPort() {
+		return serverAckPort;
+	}
+
+	public void setServerAckPort(int serverAckPort) {
+		this.serverAckPort = serverAckPort;
 	}
 }
